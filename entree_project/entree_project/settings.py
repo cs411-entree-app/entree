812ecff1e16e7b05dbb4f1b6c075acb226b03f89
Carlton Duffett <cjduffett@comcast.net>
2016-03-15 17:50:15 -0400

    Initialized PostgreSQL 9.4.5 Database
    The Entree app will connect directly to a PostgreSQL database running
    locally on the same machine as the web server. Simple and it just works.

--- entree_project/entree_project/settings.py
+++ entree_project/entree_project/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'entree_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'entreedb',
+        'USER': 'entree',
+        'PASSWORD': 'uZN4TpmsYZfK273BH8Gd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
