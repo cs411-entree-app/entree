21b17916dde0fde417a462c131028ca1060d70a7
Carlton Duffett <cjduffett@comcast.net>
2016-03-20 19:15:37 -0400

    Added entree app, base index page, and urls

--- entree_project/entree/views.py
+++ entree_project/entree/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Global site context
+context_dict = {
+    'version': '0.0.1'
+}
+
+
+def index(request):
+    return render(request, 'entree/index.html', context_dict)
--- entree_project/entree_project/settings.py
+++ entree_project/entree_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'entree',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'entree_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
