5b06d29662cd30ced063934fd6d94d0708811bcd
Carlton Duffett <cjduffett@comcast.net>
2016-03-21 16:05:15 -0400

    Added search form, posts view, about page
    I updated the index page to display a search form. When the user clicks
    the “Search” button, it POSTs the city name to the posts/ view and then
    renders a list of posts. I also added an about page to the site.

--- entree_project/entree/urls.py
+++ entree_project/entree/urls.py
@@ -3,4 +3,6 @@ from entree import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+    url(r'posts/', views.posts, name='posts'),
 ]
--- entree_project/entree/views.py
+++ entree_project/entree/views.py
@@ -8,3 +8,19 @@ context_dict = {
 
 def index(request):
     return render(request, 'entree/index.html', context_dict)
+
+
+def about(request):
+    return render(request, 'entree/about.html', context_dict)
+
+
+def posts(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+        context_dict['city'] = city
+    else:
+        print('Cannot GET this page')
+
+    return render(request, 'entree/posts.html', context_dict)
+
