f0e083b1fac22e4280adf5a3c1cff6912b656258
Carlton Duffett <cjduffett@comcast.net>
2016-03-20 22:43:35 -0400

    Added ability to toggle the default database
    You can now toggle the default database between Postgres and SQLite
    depending on your preference. Simply set the USE_POSTGRES variable in
    the settings.py file.

--- entree_project/entree_project/settings.py
+++ entree_project/entree_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# Toggle which database to use
+USE_POSTGRES = True
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,16 +80,26 @@ WSGI_APPLICATION = 'entree_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'entreedb',
-        'USER': 'entree',
-        'PASSWORD': 'uZN4TpmsYZfK273BH8Gd',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if USE_POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'entreedb',
+            'USER': 'entree',
+            'PASSWORD': 'uZN4TpmsYZfK273BH8Gd',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+
+else:
+    # use sqlite instead
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
